1dbfe9f1c37901e6ce057032b0201f91aca9997c
you@example.com <you@example.com>
2023-06-16 21:51:00 +0000

    added comments

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+# 1min = 6° , 1hr = 30°
+
 import datetime
 
-time = (datetime.datetime.now() + datetime.timedelta(hours=1)).time()
+time = (datetime.datetime.now() + datetime.timedelta(hours=1)).time() # This gets the current time in python and adds one hour for BST time
 hour = time.strftime("%I")
 minute = time.strftime("%M")
-print("Current Time: " + str(time)[:5])
 
 minute_degrees = int(minute) * 6
 hour_degrees = int(hour)* 30 + (int(minute) / 60) * 30
+
+# Displays the current time to the user along with the degrees of both the hour and minutes
+
+print("Current Time: " + str(time)[:5]) 
 
 if hour_degrees == 360:
     print(f"Hour: 0/{hour_degrees}°")
@@ -14,6 +19,6 @@ else:
     print(f"Hour: {hour_degrees}°")
 
 if minute_degrees == 0:
-    print(f"Minute: 0/360°")
+    print("Minute: 0/360°")
 else:
     print(f"Minute: {minute_degrees}°")
